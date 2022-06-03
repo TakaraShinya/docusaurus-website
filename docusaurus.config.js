b19f39529f62036c515aabd77d39b8ef89165afb
takara_shinya　[Administrator] <takara_shinya@ca-adv.co.jp>
2022-06-03 17:10:49 +0900

    config修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,13 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://takarashinya.github.io',
+  baseUrl: '/docusaurus-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'takarashinya', // Usually your GitHub org/user name.
+  projectName: 'takarashinya.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
